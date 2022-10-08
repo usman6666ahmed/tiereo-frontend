beb336fd7df473d5c9a7e7bc811adb7dd9cc65f3
usman6666ahmed <usman.imtiaz@devsinc.com>
2022-10-08 22:36:08 +0500

    Navbar
    Eslint removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import Navbar from "./components/Navbar";
 import {PublicRoutes} from "./routes";
 
 function App() {
   return (
     <div>
+      <Navbar />
       <PublicRoutes />
     </div>
   );
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <div className="navbar bg-neutral text-neutral-content">
+      <a className="btn btn-ghost normal-case text-3xl tracking-wider mx-auto">
+        Tiereo
+      </a>
+    </div>
+  );
+}
