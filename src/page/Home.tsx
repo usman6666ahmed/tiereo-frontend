a68502a26ceac5ed2d1ebc205c0594382a2ddfa4
usman6666ahmed <usman.imtiaz@devsinc.com>
2022-10-09 19:28:27 +0500

    Added props to TierList
    Installed React DND
    Prettier'd

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -6,9 +6,10 @@ export default function Home() {
   return (
     <div>
       <h1 className="text-7xl">Home</h1>
-      <TierList name="S" items={items} />
-      <TierList name="A" items={items} />
-      <TierList name="B" items={items} />
+      <TierList name="S" items={items} color="red" />
+      <TierList name="A" items={items} color="yellow" />
+      <TierList name="B" items={items} color="green" />
+      <TierList name="C" items={items} color="blue" />
     </div>
   );
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,31 +1,43 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Poppins", "sans-serif"],
-      },
-    },
+        sans: ['Poppins', 'sans-serif']
+      }
+    }
   },
-  plugins: [require("daisyui")],
+  safelist: [
+    'bg-red-800',
+    'bg-green-800',
+    'bg-blue-800',
+    'bg-yellow-800',
+    'bg-purple-800',
+    'bg-pink-800',
+    'bg-indigo-800',
+    'bg-gray-800',
+    'bg-gray-600',
+
+  ],
+  plugins: [require('daisyui')],
   daisyui: {
     themes: [
-      "light",
-      "cupcake",
+      'light',
+      'cupcake',
       {
         tiereo: {
-          primary: "#b1cc4f",
-          secondary: "#b71d43",
-          accent: "#f47733",
-          neutral: "#171E27",
-          "base-100": "#3D3A45",
-          info: "#6F90D8",
-          success: "#1B837B",
-          warning: "#C3A409",
-          error: "#F81C0D",
-        },
-      },
-    ],
-  },
-};
+          primary: '#b1cc4f',
+          secondary: '#b71d43',
+          accent: '#f47733',
+          neutral: '#171E27',
+          'base-100': '#3D3A45',
+          info: '#6F90D8',
+          success: '#1B837B',
+          warning: '#C3A409',
+          error: '#F81C0D'
+        }
+      }
+    ]
+  }
+}
